3fe20c7ca2613531da7b5b3d03b04ab392292648
Mori Ahmadi <ahmadik.morteza95@gmail.com>
2024-08-04 20:45:00 -0400

    Initial Commit

---
+++ Corner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Corner
+//
+//  Created by Mori Ahmadi on 2024-08-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Corner/CornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CornerApp.swift
+//  Corner
+//
+//  Created by Mori Ahmadi on 2024-08-04.
+//
+
+import SwiftUI
+
+@main
+struct CornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
