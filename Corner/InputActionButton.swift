1ad544eb54acf9ef7af2a30e70e00852b7f91dd2
Mori Ahmadi <ahmadik.morteza95@gmail.com>
2024-08-11 14:03:44 -0400

    Add input action button

--- Corner/InputActionButton.swift
+++ Corner/InputActionButton.swift
@@ -5,4 +5,25 @@
 //  Created by Mori Ahmadi on 2024-08-11.
 //
 
-import Foundation
+import SwiftUI
+
+struct ActionButton: View {
+    let title: String
+    let color: Color
+    let action: () -> Void
+    
+    var body: some View {
+        Button(action: action) {
+            Text(title)
+                .frame(maxWidth: .infinity, alignment: .center)
+                .padding()
+                .foregroundStyle(.background)
+                .background {
+                    RoundedRectangle(cornerRadius: 12)
+                        .fill(color)
+                        .shadow(radius: 4)
+                }
+        }
+        .buttonStyle(.borderless)
+    }
+}
