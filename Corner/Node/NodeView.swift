9c944c769ca8beb7c7f8171e0a76cda94b0a77ce
Mori Ahmadi <ahmadik.morteza95@gmail.com>
2024-09-23 23:25:37 -0400

    add description to node view

--- Corner/Node/NodeView.swift
+++ Corner/Node/NodeView.swift
@@ -12,27 +12,40 @@ struct NodeView: View {
     let node: Node
     
     var body: some View {
-        Text(node.id)
-            .font(.headline)
-            .fontWeight(.semibold)
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .padding(UXMetrics.Padding.twenty)
-            .foregroundColor(node.color)
-            .background {
-                RoundedRectangle(cornerRadius: UXMetrics.CornerRadius.eight)
-                    .fill(
-                        LinearGradient(
-                            colors: [topGradientColor, node.color.opacity(0.2)],
-                            startPoint: .top,
-                            endPoint: UnitPoint(x: 0.5, y: 4)
-                        )
-                    )
-                    .shadow(color: node.color.opacity(0.2), radius: UXMetrics.ShadowRadius.four)
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+        VStack(alignment: .leading, spacing: 2) {
+            Text(node.id)
+                .font(.headline)
+                .fontWeight(.semibold)
+                .foregroundColor(node.color)
+                .frame(maxWidth: .infinity, alignment: doesHaveDesc ? .leading : .center)
+            if let desc = node.desc {
+                Text(desc)
+                    .font(.caption)
+                    .fontWeight(.regular)
+                    .foregroundColor(.primary)
+                    .frame(maxWidth: 200, alignment: .topLeading)
             }
+        }
+        .padding(UXMetrics.Padding.twenty)
+        .background {
+            RoundedRectangle(cornerRadius: UXMetrics.CornerRadius.eight)
+                .fill(
+                    LinearGradient(
+                        colors: [topGradientColor, node.color.opacity(0.2)],
+                        startPoint: .top,
+                        endPoint: UnitPoint(x: 0.5, y: 4)
+                    )
+                )
+                .shadow(color: node.color.opacity(0.2), radius: UXMetrics.ShadowRadius.four)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+        }
     }
     
     private var topGradientColor: Color {
         colorScheme == .dark ? .black : .white
+    }
+    
+    private var doesHaveDesc: Bool {
+        return node.desc != nil
     }
 }
